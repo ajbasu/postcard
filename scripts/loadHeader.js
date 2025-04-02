a522f6c72f0bbe9a642d77594e7438bd1f58d5fc
Omolan <amlan286@gmail.com>
2025-04-02 19:40:33 +0530

    made header responsive

--- scripts/loadHeader.js
+++ scripts/loadHeader.js
@@ -5,8 +5,17 @@ document.addEventListener("DOMContentLoaded", function () {
             document.getElementById("header-container").innerHTML = html;
             // If window.pageTitle is defined, update the header title
             if (window.pageTitle) {
-                document.getElementById("fotw-header-title").textContent = "redteapost | " + window.pageTitle;
+                document.getElementById("fotw-header-title").textContent = window.pageTitle;
             }
         })
         .catch(error => console.error("Error loading header:", error));
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.getElementById("fotw-menu-toggle");
+    const navMenu = document.querySelector(".fotw-header-right nav");
+
+    menuToggle.addEventListener("click", function () {
+        navMenu.classList.toggle("active");
+    });
 });
